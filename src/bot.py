4c0793c3d74ceca6791b76b3ce41a5ad51e20e97
Maks <max.centeno64@gmail.com>
2022-09-16 18:39:06 +1000

    DONE

---
+++ src/bot.py
@@ -0,0 +1,50 @@
+import logging
+import os
+import coloredlogs
+import motor.motor_asyncio
+import disnake
+from disnake.ext import commands
+from dotenv import load_dotenv
+from pathlib import Path
+from pymongo import MongoClient
+
+log = logging.getLogger("Log Bot")
+coloredlogs.install(logger=log)
+
+
+class LogBot(commands.Bot):
+    def __init__(self):
+        load_dotenv()
+        intents = disnake.Intents.default()
+
+        super().__init__(reload=True, intents=intents)
+
+        logging.basicConfig(
+            level=logging.INFO,
+            format="(%(asctime)s) %(levelname)s %(message)s",
+            datefmt="%m/%d/%y - %H:%M:%S %Z"
+        )
+        self.loop.create_task(self.prepare_bot())
+
+    async def prepare_bot(self):
+        log.info("Connecting to database...")
+        # self.mongo = motor.motor_asyncio.AsyncIOMotorClient(
+        #     os.getenv("MONGO"))        
+        log.info("Database connection established.")
+
+    def load_cogs(self):
+        for file in os.listdir(str(Path(__file__).parents[0]) + "/cogs"):
+            if file.endswith(".py") and not file.startswith("_"):
+                self.load_extension(f"cogs.{file[:-3]}")
+
+        log.info("All cogs have been loaded. ")
+
+    def run(self):
+        if not os.getenv("BOT_TOKEN") or os.getenv("BOT_TOKEN") == "":
+            return log.error("No .env file setup with proper token paramter.")
+        self.load_cogs()
+        super().run(os.getenv("BOT_TOKEN"))
+
+
+bot = LogBot()
+bot.run()
---
+++ src/cogs/fetch.py
@@ -0,0 +1,51 @@
+import logging
+import os
+from typing import Literal
+import coloredlogs
+import disnake
+from disnake.ext import commands
+import pymongo
+try:
+    test_guilds = [int(os.getenv("TEST_GUILD"))]
+finally:
+    test_guilds = []
+
+log = logging.getLogger("Log cog")
+coloredlogs.install(logger=log)
+
+class LogFind(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        log.warn(f"{self.__class__.__name__} Cog has been loaded")  
+
+    @commands.slash_command(name="fetch", guild_ids=test_guilds, description="Fetches modmail log from the database")
+    @commands.guild_only()
+    async def test(self, inter: disnake.ApplicationCommandInteraction, logkey: str) -> None:
+        await inter.response.defer()
+        myclient = pymongo.MongoClient(os.getenv("MONGO"))
+        mydb = myclient["modmail_bot"]
+        mycol = mydb["logs"]
+        data = mycol.find_one({"key": logkey})
+        if data is None:
+            await inter.followup.send("Log not found") 
+            return
+        avatar = data['recipient']['avatar_url']
+        messages = data['messages']
+        embed = disnake.Embed(title=f"{data['recipient']['name']}({data['recipient']['id']})", color=disnake.Color.blurple())
+        embed2 = disnake.Embed(color=disnake.Color.blurple())    
+        embed.set_author(name="Modmail Log", icon_url=avatar)
+        for x in messages:
+            embed.add_field(name=f"`{x['author']['name']}({x['author']['id']})`", value=f"{x['content']}", inline=False)
+            
+        await inter.edit_original_message(embeds=[embed])
+ 
+        for y in messages[31:]:
+            embed2.add_field(name=f"`{y['author']['name']}({y['author']['id']})`", value=f"{y['content']}", inline=False)
+            
+        await inter.send(embed=embed2)            
+
+def setup(bot: commands.Bot):
+    bot.add_cog(LogFind(bot))
